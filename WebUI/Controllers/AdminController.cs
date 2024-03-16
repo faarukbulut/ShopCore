df20948505dd7ce6529f0452235418b628374434
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-03-16 10:58:04 +0300

    Yeni ürün ekleme

--- WebUI/Controllers/AdminController.cs
+++ WebUI/Controllers/AdminController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebUI.Controllers
 {
     public class AdminController : Controller
     {
-        public IActionResult Index()
+        private readonly IProductService _productService;
+
+        public AdminController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IActionResult CreateProduct()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateProduct(Product p)
+        {
+            _productService.Create(p);
+            return RedirectToAction("List", "Shop");
         }
     }
 }
