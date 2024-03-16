06f985a40c97852f86185fa0255a46c4144fec89
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-03-16 10:09:00 +0300

    Kategori Listesi ViewCompoenent

---
+++ WebUI/ViewComponents/_CategoryListViewComponentPartial.cs
@@ -0,0 +1,21 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents
+{
+    public class _CategoryListViewComponentPartial : ViewComponent
+    {
+        private readonly ICategoryService _categoryService;
+
+        public _CategoryListViewComponentPartial(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _categoryService.GetAll();
+            return View(values);
+        }
+    }
+}
