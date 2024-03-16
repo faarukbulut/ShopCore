7b3b3589b664a90de6c9df4acebeda0b1cf94343
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-03-16 08:56:37 +0300

    Ürünlerin Listelenmesi Default&Shop

--- WebUI/Controllers/ShopController.cs
+++ WebUI/Controllers/ShopController.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebUI.Controllers
 {
     public class ShopController : Controller
     {
+        private readonly IProductService _productService;
+
+        public ShopController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult List()
+        {
+            var values = _productService.GetAll();
+            return View(values);
         }
     }
 }
